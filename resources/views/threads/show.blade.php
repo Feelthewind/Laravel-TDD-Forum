e853ac063326f38b8008007a146015bbb3fe1d38
lcs <skyisthelimitlessW@gmail.com>
2019-03-07 00:34:08 +0900

    ep3

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -13,5 +13,23 @@
             </div>
         </div>
     </div>
+
+    <div class="row justify-content-center">
+      @foreach ($thread->replies as $reply)
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header">
+              <a href="#">
+                {{ $reply->owner->name }}
+              </a> said {{ $reply->created_at->diffForHumans() }}...
+            </div>
+
+            <div class="card-body">
+              {{ $reply->body }}
+            </div>
+          </div>
+        </div>
+      @endforeach
+    </div>
 </div>
 @endsection
---
+++ tests/Unit/ThreadTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ThreadTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_thread_has_replies()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $thread->replies);
+    }
+}
