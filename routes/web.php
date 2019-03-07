c5a9a6f5eee2ebd76c7bca91708e689e1fbd006b
lcs <skyisthelimitlessW@gmail.com>
2019-03-07 14:41:16 +0900

    ep8

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/threads', 'ThreadsController@index');
-Route::get('/threads/{thread}', 'ThreadsController@show');
-Route::post('/threads', 'ThreadsController@store');
+// Route::get('/threads', 'ThreadsController@index');
+// Route::get('/threads/{thread}', 'ThreadsController@show');
+// Route::post('/threads', 'ThreadsController@store');
+Route::resource('threads', 'ThreadsController');
 
 Route::post('/threads/{thread}/replies', 'RepliesController@store');
 
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,10 +3,19 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use App\Exceptions\Handler;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->disableExceptionHandling();
+    }
 
     protected function signIn($user = null)
     {
@@ -14,6 +23,31 @@ abstract class TestCase extends BaseTestCase
 
         $this->actingAs($user);
 
+        return $this;
+    }
+
+    // Hat tip, @adamwathan.
+    protected function disableExceptionHandling()
+    {
+        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+        $this->app->instance(
+            ExceptionHandler::class,
+            new class extends Handler
+            {
+                public function __construct()
+                { }
+                public function report(\Exception $e)
+                { }
+                public function render($request, \Exception $e)
+                {
+                    throw $e;
+                }
+            }
+        );
+    }
+    protected function withExceptionHandling()
+    {
+        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
         return $this;
     }
 }
