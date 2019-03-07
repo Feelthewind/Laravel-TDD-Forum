a3568e100259ce08a813186e3515bb479fdd527e
lcs <skyisthelimitlessW@gmail.com>
2019-03-07 22:00:20 +0900

    ep16

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -14,33 +14,39 @@
                   {{ $thread->body }}
                 </div>
             </div>
+
+            @foreach ($replies as $reply)
+              @include('threads.reply')
+            @endforeach
+
+            {{ $replies->links() }}
+
+            @if (auth()->check())
+              <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                {{ csrf_field() }}
+
+                <div class="form-group">
+                  <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-default">Post</button>
+              </form>
+            @else
+              <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+            @endif
         </div>
-    </div>
-
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        @foreach ($thread->replies as $reply)
-          @include('threads.reply')
-        @endforeach
-      </div>
-    </div>
-
-    @if (auth()->check())
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <form method="POST" action="{{ $thread->path() . '/replies' }}">
-            {{ csrf_field() }}
-
-            <div class="form-group">
-              <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+        
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <p>
+                This thread was published {{ $thread->created_at->diffForHumans() }} by
+                <a href="#">{{ $thread->creator->name }}</a>, and currently
+                has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
+              </p>
             </div>
-
-              <button type="submit" class="btn btn-default">Post</button>
-          </form>
+          </div>
         </div>
-      </div>
-    @else
-        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
-    @endif
+    </div>
 </div>
 @endsection
