2df122e63b13b85dc0944a61f7b0c24ccfba47c1
lcs <skyisthelimitlessW@gmail.com>
2019-03-07 14:02:48 +0900

    ep7

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function guests_may_not_create_threads()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $thread = make('App\Thread');
+
+        $this->post('/threads', $thread->toArray());
+    }
+
+    /** @test */
+    function an_authenticated_user_can_create_new_forum_threads()
+    {
+        $this->signIn();
+
+        $thread = make('App\Thread');
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
--- tests/Feature/ParticipateInThreadsTest.php
+++ tests/Feature/ParticipateInThreadsTest.php
@@ -21,13 +21,14 @@ class ParticipateInThreadsTest extends TestCase
     /** @test */
     function an_authenticated_user_may_participate_in_forum_threads()
     {
-        $this->be(factory('App\User')->create());
+        $this->signIn();
 
-        $thread = factory('App\Thread')->create();
-        $reply = factory('App\Reply')->make();
+        $thread = create('App\Thread');
+        $reply = create('App\Reply');
 
         $this->post($thread->path() . '/replies', $reply->toArray());
 
-        $this->get($thread->path())->assertSee($reply->body);
+        $this->get($thread->path())
+            ->assertSee($reply->body);
     }
 }
