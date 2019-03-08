3854390cd0db26aa091d75e9f61c732daa6dbf04
lcs <skyisthelimitlessW@gmail.com>
2019-03-08 17:45:57 +0900

    ep18

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
 Route::post('/threads', 'ThreadsController@store');
 Route::get('/threads/{channel}', 'ThreadsController@index');
 
+Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
+
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
 
 Auth::routes();
