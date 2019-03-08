16ea29e4db01a9649e5796c63e3f636823c68a81
lcs <skyisthelimitlessW@gmail.com>
2019-03-08 19:16:08 +0900

    ep21

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+        }
     }
 
     /**
@@ -25,7 +27,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \View::composer('*', function ($view) {
-            $view->with('channels', Channel::all());
+            $channels = \Cache::rememberForever('channels', function () {
+                return Channel::all();
+            });
+            $view->with('channels', $channels);
         });
     }
 }
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,22 @@
 <div class="card">
   <div class="card-header">
-    <a href="#">
-      {{ $reply->owner->name }}
-    </a> said {{ $reply->created_at->diffForHumans() }}...
+    <div class="level">
+      <h5 class="flex">
+        <a href="#">
+          {{ $reply->owner->name }}
+        </a> said {{ $reply->created_at->diffForHumans() }}...
+      </h5>
+
+      <div>
+        <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+          {{ csrf_field() }}
+
+          <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+            {{ $reply->favorites_count }} {{ str_plural('Favorite', $reply->favorites_count) }}
+          </button>
+        </form>
+      </div>
+    </div>
   </div>
 
   <div class="card-body">
